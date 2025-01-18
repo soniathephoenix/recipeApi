0fa6c0b596f3db250dcaf4b866f9919c731c7dfc
Sonia Pires <soniacodesloads@gmail.comgit config --global user.name soniathephoenix>
2025-01-18 10:24:47 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+//Api - Filter by main ingredient: www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
+
+const prompt=require("prompt-sync")({sigint:true}); 
+
+let answer = prompt('Would you like to roll a dice? (yes/no):').toLowerCase();
