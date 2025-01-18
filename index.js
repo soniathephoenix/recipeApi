97e40468b2e04137bb599e98fcb5c3d9bd8ca96c
Sonia Pires <soniacodesloads@gmail.comgit config --global user.name soniathephoenix>
2025-01-18 10:50:56 +0000

    add title and search bar

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 //Api - Filter by main ingredient: www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
 
-const prompt=require("prompt-sync")({sigint:true}); 
+const title = document.getElementById('title')
 
-let answer = prompt('Would you like to roll a dice? (yes/no):').toLowerCase();
+title.textContent = "Let's get cooking!"
